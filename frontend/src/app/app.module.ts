a8815a8febfe6be99dc1906577c7328440685ebf
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-09-13 15:30:01 -0500

    Header y Footer
    1.- Se creó el Header con Bootstrap.
    2.- Se creó el Footer.
    3.- Se instaló Bootstrap.
    4.- Se inicializó el ruteo de las vistas.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes : Routes = [
+  {path:'', component: InicioComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
