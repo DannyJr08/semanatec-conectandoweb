73be92251996ea0ef70284e387f90881600023a7
5100-chap <A01275100@tec.mx>
2022-09-14 12:00:26 -0500

    Añadido Hash para contraseña en post usuario

--- API/CRUD/index.js
+++ API/CRUD/index.js
@@ -4,9 +4,21 @@ const cors = require("cors");
 const firebase = require("firebase/app");
 const Ent = require("./config.js");
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+//Funcion Hash de dominio publico 
+const cyrb53 = function(str, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1>>>0);
+};
 
 //GET (Read)
 app.get("/user", async (req, res) => {
@@ -29,7 +41,12 @@ app.get("/reminder", async (req, res) => {
 //POST (Create)
 app.post("/createUser", async (req, res) => {
     const data = req.body;
-    await Ent.Usuario.add(data);
+    await Ent.Usuario.add({
+        email: data.email,
+        hash: cyrb53(data.hash, 5),
+        signature: data.signature,
+        id: data.id
+    });
     res.send({ msg: "Usuario registrado correctamente" })
 });
 app.post("/createList", async (req, res) => {
