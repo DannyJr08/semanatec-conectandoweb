59de08a5c9366179621b3bb71dc191c99cad1d51
5100-chap <A01275100@tec.mx>
2022-09-13 19:11:01 -0500

    Funcion GET creado

--- API/CRUD/index.js
+++ API/CRUD/index.js
@@ -5,10 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", async (req, res) => {
+    const snapshot = await Usuario.get();
+    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    res.send(list);
+});
+
 app.post("/createUser", async (req, res) => {
     const data = req.body
     await Usuario.add(data)
-    res.send({msg: "Usuario registrado correctamente"})
+    res.send({ msg: "Usuario registrado correctamente" })
 });
 
 app.listen(8080, () => console.log("Server iniciado en el puerto 8080"))
