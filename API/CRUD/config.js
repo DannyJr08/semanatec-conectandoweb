e4beae1a8f4754ed0c05b8797b8c7402addce4f2
5100-chap <A01275100@tec.mx>
2022-09-13 19:54:26 -0500

    Avance en el API y añadido entidades faltantes

--- API/CRUD/config.js
+++ API/CRUD/config.js
@@ -1,5 +1,6 @@
+// Libreria a llamar
 const firebase = require("firebase");
-
+//Credenciales
 const firebaseConfig = {
     apiKey: "AIzaSyBXMJoTNoHT9hJwL1qGNEzLeb_uCb9Krw8",
     authDomain: "semana-tec-web-6.firebaseapp.com",
@@ -9,9 +10,18 @@ const firebaseConfig = {
     messagingSenderId: "761986698524",
     appId: "1:761986698524:web:6283eb7068bac4e40ebed9",
 };
-
+//Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
+//Define la base de datos
 const db = firebase.firestore();
-const Usuario = db.collection("Usuario");
-module.exports = Usuario;
 console.log("Conectado a la base de datos.")
+//Define nuestras "Colecciones(Entidades)"
+const Usuario = db.collection("Usuario");
+const Lista = db.collection("Lista");
+const Recordatorio = db.collection("Recordatorio");
+//Exportamos nuestras identidades para que otros archivos
+//Puedan interactuar con estas
+module.exports.Usuario = Usuario;
+module.exports.Lista = Lista;
+module.exports.Recordatorio = Recordatorio;
+console.log("Colecciones locales cargadas.")
--- API/CRUD/index.js
+++ API/CRUD/index.js
@@ -1,20 +1,35 @@
+//Librerias a usar
 const express = require("express");
 const cors = require("cors");
-const Usuario = require("./config.js");
+const Ent = require("./config.js");
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-
+//GET (Read)
 app.get("/", async (req, res) => {
-    const snapshot = await Usuario.get();
+    const snapshot = await Ent.Lista.get();
     const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
     res.send(list);
 });
-
+//POST (Create)
 app.post("/createUser", async (req, res) => {
-    const data = req.body
-    await Usuario.add(data)
+    const data = req.body;
+    await Ent.Usuario.add(data);
     res.send({ msg: "Usuario registrado correctamente" })
 });
-
+//Update
+app.post("/update", async (req, res) => {
+    const id = req.body.id;
+    delete req.body.id;
+    const data = req.body;
+    await  Ent.Usuario.doc(id).update(data);
+    res.send({ msg: "Usuario actualizado" });
+});
+// Delete
+app.post("/delete", async (req, res) => {
+    const id = req.body.id;
+    await  Ent.Usuario.doc(id).delete();
+    res.send({ msg: "Usuario Eliminado" });
+});
 app.listen(8080, () => console.log("Server iniciado en el puerto 8080"))
