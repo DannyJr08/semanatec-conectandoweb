ba5892fe3009484a48b7438a68d5d53bac7c9b5d
5100-chap <A01275100@tec.mx>
2022-09-14 22:42:37 -0500

    Co-authored-by: BrendaSaucedo12 <BrendaSaucedo12@users.noreply.github.com>

--- API/CRUD/index.js
+++ API/CRUD/index.js
@@ -22,56 +22,111 @@ const cyrb53 = function(str, seed = 0) {
 
 //GET (Read)
 app.get("/user", async (req, res) => {
-    const snapshotU = await Ent.Usuario.get();
-    const listU = snapshotU.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.send(listU);
+    try {
+        const snapshotU = await Ent.Usuario.get();
+        const listU = snapshotU.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        res.send(listU);
+        console.log("GET de todos los usuarios con exito");
+    } catch (error) {
+        console.log("Error en GET todos los usuarios");
+    }
+});
+app.get("/user/:email", async (req, res) => {
+    try {
+        const emailUser = req.params.email
+        const snapshotU = await Ent.Usuario.get();
+
+        snapshotU.forEach(doc => {
+            var data = doc.data();
+            if (data.email == emailUser) {
+                Ent.Usuario.doc(`${doc.id}`).delete();
+                mns = "Usuario Eliminado"
+            }
+        });
+
+        const listU = snapshotU.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        res.send(listU);
+        console.log("GET de usuarios con exito");
+    } catch (error) {
+        console.log("Error en GET usuarios");        
+    }
+
 });
 app.get("/list", async (req, res) => {
-    const snapshot = await Ent.Lista.get();
-    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.send(list);
+    try {
+        const snapshot = await Ent.Lista.get();
+        const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        res.send(list);
+        console.log("GET de todas las listas con exito");
+    } catch (error) {
+        console.log("Error en GET de todas las listas");
+    }
+
 });
 app.get("/reminder", async (req, res) => {
-    const snapshotR = await Ent.Recordatorio.get();
-    const listR = snapshotR.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.send(listR);
+    try {
+        const snapshotR = await Ent.Recordatorio.get();
+        const listR = snapshotR.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        res.send(listR);
+        console.log("GET de todas los recordatorios con exito");
+    } catch (error) {
+        console.log("Error en GET todos los recordatorios");
+    }
+
 });
 
 
 //POST (Create)
 app.post("/createUser", async (req, res) => {
-    const data = req.body;
-    const snapshot = await Ent.Usuario.where("email", "==", data.email).get();
-    const does_exist = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    if (does_exist.length == 0 ){
-        await Ent.Usuario.add({
-            email: data.email,
-            hash: cyrb53(data.hash, 5),
-            signature: data.signature
-        });
-        res.send({ msg: "Usuario registrado correctamente" })
-    }
-    else{
-        res.send({ msg: "Usuario ya existente"})
+    try {
+        const data = req.body;
+        const snapshot = await Ent.Usuario.where("email", "==", data.email).get();
+        const does_exist = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        if (does_exist.length == 0 ){
+            await Ent.Usuario.add({
+                email: data.email,
+                hash: cyrb53(data.hash, 5),
+                signature: data.signature
+            });
+            res.send({ msg: "Usuario registrado correctamente" });
+            console.log("Usuario registrado correctamente");
+        }
+        else{
+            res.send({ msg: "Usuario ya existente"});
+            console.log("Usuario ya existente");
+        }
+    } catch (error) {
+        console.log("Error en POST de usuario");
     }
     
 });
 app.post("/createList", async (req, res) => {
-    const dataL = req.body;
-    await Ent.Lista.add(dataL);
-    res.send({ msg: "Lista registrada correctamente" })
+    try {
+        const dataL = req.body;
+        await Ent.Lista.add(dataL);
+        res.send({ msg: "Lista registrada correctamente" });
+        console.log("Lista registrada correctamente");
+    } catch (error) {
+        console.log("Error en POST de Lista")
+    }
+
 });
 app.post("/createReminder", async (req, res) => {
-    const dataR = req.body;
-    await Ent.Recordatorio.add({
-        id_list: dataR.id_list,
-        content : dataR.content,
-        day : dataR.day,
-        month : dataR.month,
-        year : dataR.year,
-        Timestamp : Date.now()
-    });
-    res.send({ msg: "Recordatorio registrado correctamente" })
+    try {
+        const dataR = req.body;
+        await Ent.Recordatorio.add({
+            id_list: dataR.id_list,
+            content : dataR.content,
+            day : dataR.day,
+            month : dataR.month,
+            year : dataR.year,
+            Timestamp : Date.now()
+        });
+        res.send({ msg: "Recordatorio registrado correctamente" });
+        console.log("Recordatorio registrado correctamente");
+    } catch (error) {
+        console.log("Error en POST de Recordatorio");
+    }
 });
 
 //Update
@@ -108,7 +163,7 @@ app.delete("/deleteReminder", async (req, res) => {
     // const date = dateComplete[0].split("-");
     // const hour = dateComplete[1];
 
-    
+
     // console.log(dateFirebase);
 
     var mns = "Recordatorio No Existente";
@@ -151,4 +206,4 @@ app.delete("/deleteList", async (req, res) => {
     res.send({ msg: mns });
 });
 
-app.listen(8080, () => console.log("Server iniciado en el puerto 8080"))
+app.listen(8080, () => console.log("Server iniciado en el puerto 8080"));
